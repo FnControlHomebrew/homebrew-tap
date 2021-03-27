c9a99d68771d81f39db02e376a9365e0f4124013
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-03-27 10:11:58 -0700

    Update goplus.rb

--- Formula/goplus.rb
+++ Formula/goplus.rb
@@ -9,8 +9,8 @@ class Goplus < Formula
   depends_on "go" => :build
 
   def install
-    # ENV["GOPATH"] = buildpath
-    # ENV["GOBIN"] = bin
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = bin
     ENV["GO111MODULE"] = "auto"
 
     system "go", "install", "-v", "./..."
