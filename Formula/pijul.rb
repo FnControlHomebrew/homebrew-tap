57858b495dc5a3bc5b966dd63a45a2303701dc06
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-10-28 21:35:24 +0000

    pijul: add 1.0.0-alpha.55 bottle.

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -10,6 +10,13 @@ class Pijul < Formula
     regex(/"num":\s*"(\d+(?:\.\d+|[._-](?:alpha|beta))+)"/i)
   end
 
+  bottle do
+    root_url "https://github.com/FnControlOption/homebrew-tap/releases/download/pijul-1.0.0-alpha.55"
+    sha256 cellar: :any,                 big_sur:      "4230c54ddea3ad976e5e49727379df87001ec1596bf9c44da0b4028918bf77e5"
+    sha256 cellar: :any,                 catalina:     "e1c583d4fe5725b25bcb6e63e616f230b356f4d2d824ef7ae3be20ffdfda7040"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "61a1ee2370c729358ae6440eac10f74380efa151b0ce3fa929fea3508277381e"
+  end
+
   depends_on "rust" => :build
   depends_on "libgit2"
   depends_on "libsodium"
