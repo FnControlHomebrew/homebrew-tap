e7956d9e0c7b8f05fd6d4846722a5948f4480938
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-19 03:03:44 -0800

    Formula

---
+++ Formula/foobar.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Foobar < Formula
+  desc "Minimal C program and Makefile used for testing Homebrew"
+  homepage "https://github.com/Homebrew/brew"
+  url "file://#{Tap.fetch("homebrew", "test-bot").formula_dir}/tarballs/testbottest-0.1.tbz"
+  sha256 "246c4839624d0b97338ce976100d56bd9331d9416e178eb0f74ef050c1dbdaad"
+  license "BSD-2-Clause"
+  head "https://github.com/Homebrew/homebrew-test-bot.git"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  def post_install
+    system "#{bin}/testbottest"
+  end
+
+  test do
+    assert_equal "testbottest\n", shell_output("#{bin}/testbottest")
+  end
+end
